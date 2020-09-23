a4f0a67fa39ab83222fbf31de36056d7809d1102
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-24 02:32:51 +0500

    useing double click func

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ const App = () => {
 
     const purple = "#8e44ad"
  const [bg, setBg] = useState(purple);
+ const [name , setName] = useState("click me")
     const bgChange = () => {
         let newBg = "#34495e"
         setBg(newBg);
+        setName ("Och !! ")
+    };
+
+    const bgBack = () => {
+        setBg(purple);
+        setName ("jay mata Dii !!!!" );
+
     };
 
 return(
     <>
         <div style = {{ backgroundColor: bg }}>
-            <button onClick = {bgChange} >Click Me</button>
+            <button onClick = {bgChange} onDoubleClick={bgBack} > {name} </button>
         </div>
     </>
 );
